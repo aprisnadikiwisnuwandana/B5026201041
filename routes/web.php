66931f51fda3ed52bbbadf8226e6ed1e038e99f9
aprisnadikiwisnuwandana <aprisnadiki@gmail.com>
2021-11-15 18:35:18 +0700

    Input, Update, Delete

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,11 @@ Route::post('php', 'ViewController@showPhp');
 
 //route CRUD
 Route::get('/pegawai','PegawaiController@index');
+
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
